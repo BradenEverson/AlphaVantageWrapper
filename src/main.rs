e040fb8461aca1a2aa56a5bc1b97e4b5184bdddd
= <=>
2023-10-13 16:42:56 -0500

    Added file path

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod lib;
 
-
-
+use lib::broker_api::BrokerAPI;
 use reqwest::Error;
-
-
 
 #[tokio::main]
 async fn main() -> Result<(), Error>{
+    let mut api = BrokerAPI::new("key");
     Ok(())
 }
