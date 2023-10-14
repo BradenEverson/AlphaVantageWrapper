4d8805f3a93e1d74454500b81e1def84e1df1ef2
= <=>
2023-10-14 04:10:41 -0500

    Changed naming to adhere to rust

--- src/broker/broker_response.rs
+++ src/broker/broker_response.rs
@@ -1,11 +1,11 @@
 use crate::broker::response_types::parsed_data::TimeSeries;
 
 pub struct BrokerResponse{
-    pub TimeSeriesData: Vec<TimeSeries>
+    pub time_series_data: Vec<TimeSeries>
 }
 
 impl BrokerResponse{
     pub fn new() -> BrokerResponse{
-        BrokerResponse{TimeSeriesData: vec![]}
+        BrokerResponse{time_series_data: vec![]}
     }
 }
