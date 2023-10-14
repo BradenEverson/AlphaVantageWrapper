21b063739965e6e278675a7197017077d98e425a
= <=>
2023-10-14 05:58:23 -0500

    Added clone support for TimeSeries

--- src/broker/response_types/parsed_data.rs
+++ src/broker/response_types/parsed_data.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct TimeSeries{
     pub time_stamp: String,
 
